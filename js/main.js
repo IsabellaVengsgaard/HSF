d3d0379d9395705d1a0052c53b7305f4281d3139
Isabella Vengsgaard <eaaimbv@students.eaaa.dk>
2019-05-27 11:12:43 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -154,3 +154,22 @@ function getFeaturedImageUrl(post) {
   }
   return imageUrl;
 }
+
+
+
+//script til menusiden mobil hvor der skal slide mere infomation ned når der klikkes
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
