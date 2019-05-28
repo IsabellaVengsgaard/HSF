37f853f5d302c00921b917d775a91bb58ede5744
Isabella Vengsgaard <eaaimbv@students.eaaa.dk>
2019-05-28 10:18:13 +0200

    hiuhu

--- js/main.js
+++ js/main.js
@@ -6,8 +6,10 @@ var accordions = document.getElementsByClassName("accordion");
 
 for (var i = 0; i < accordions.length; i++) {
     accordions[i].onclick = function () {
+        this.classList.toggle('is-open');
         var content =  this.nextElementSibling;
-        
+        var plusIcon = document.getElementById('')
+
         if (content.style.maxHeight) {
         //accordion is open, we need to close it
             content.style.maxHeight = null;
